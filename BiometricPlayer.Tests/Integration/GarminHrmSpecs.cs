3a9a12651869622897efb1fac331a779b4c5cbc6
Igor Khavkin <ikhavkin@gmail.com>
2013-01-28 00:55:39 +0200

    prepared ugly test that relies on timing, now to figure out how to handle it with TestScheduler

--- BiometricPlayer.Tests/Integration/GarminHrmSpecs.cs
+++ BiometricPlayer.Tests/Integration/GarminHrmSpecs.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
+using Autofac.Core;
 using BiometricPlayer.Core;
 using FluentAssertions;
 using Machine.Specifications;
@@ -13,28 +16,103 @@ namespace BiometricPlayer.Tests.Integration
         static IAntChannel channel;
 
         Establish context = () =>
-            {
-                device.NetworkKey = new byte[] { 0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45 };
-                device.Init();
-                channel = device.Channel;
-            };
+                                {
+                                    device.NetworkKey = new byte[] {0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45};
+                                    device.Init();
+                                    channel = device.Channel;
+                                };
 
         Because of = () =>
-            {
-                channel.ChannelPeriod = 0x1f86;
-                channel.ChannelRFFrequency = 0x39;
+                         {
+                             channel.ChannelPeriod = 0x1f86;
+                             channel.ChannelRFFrequency = 0x39;
 
-                channel.Open();
-            };
+                             channel.Open();
+                         };
 
         It should_not_throw = () => { };
+
         It should_raise_channel_events = () =>
-                                            {
-                                                var events = channel.Events.Take(40).ToEnumerable();
-                                                string[] dataAsStrings = 
-                                                    events.Select(message => BitConverter.ToString(message.Data.ToArray())).ToArray();
-                                                dataAsStrings.Should().HaveCount(40);
-                                            };
+                                             {
+                                                 var events = channel.Events.Take(40).ToEnumerable();
+                                                 string[] dataAsStrings =
+                                                     events.Select(
+                                                         message => BitConverter.ToString(message.Data.ToArray()))
+                                                           .ToArray();
+                                                 dataAsStrings.Should().HaveCount(40);
+                                             };
+    }
+
+    [Subject(typeof (MockAntChannel))]
+    public class When_listening_hrm_channel_for_65_messages : MockHrmChannelSpec
+    {
+        static IEnumerable<AntMessage> messages;
+
+        Because of = () =>
+            messages = channel.Events.Take(65).TakeUntil(DateTimeOffset.Now + TimeSpan.FromSeconds(2)).ToEnumerable();
+
+        It should_not_fail = () => { };
+
+        It should_return_65_messages = () =>
+            messages.Should().HaveCount(65);
+
+        It should_contain_alternating_highest_bit_of_page_byte = () =>
+            {
+                var pageHighestBytes = messages.Select(m => m.Data[1] & 0x80); // todo: check why it's on the index 1
+                pageHighestBytes.Should().Contain(0x00).And.Contain(0x80);
+            };
+    }
+
+    public class MockHrmChannelSpec
+    {
+        protected static IAntChannel channel;
+
+        Establish context = () =>
+                                {
+                                    channel = new MockAntChannel();
+                                };
+    }
+
+    public class MockAntChannel : IAntChannel
+    {
+        public static readonly TimeSpan Period = TimeSpan.FromSeconds(0.25); //TimeSpan.FromTicks(10000000L * 8070 / 32768);
+
+        static readonly byte[][] Messages = new[]
+            {
+                new byte[] {0x00, 0x04, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x04, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x04, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x04, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+
+                new byte[] {0x00, 0x84, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x84, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x84, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39},
+                new byte[] {0x00, 0x84, 0x00, 0x72, 0x35, 0xB0, 0x39, 0x12, 0x39}
+            };
+
+        public IObservable<AntMessage> Events
+        {
+            get
+            {
+                var messagesLoop = Messages.ToObservable().Repeat();
+
+//                return messagesLoop.Zip(Observable.Interval(Period)).
+//                    Select(t => new AntMessage(0x4e, t.Item1));
+                return messagesLoop.Zip(Observable.Interval(Period), (bytes, i) => bytes).
+                    Select(bytes => new AntMessage(0x4e, bytes));
+//                return Observable.Interval(Period).Select(i => new AntMessage(0x4e, Messages[i % Messages.Length]));
+            }
+        }
+
+        public byte ChannelRFFrequency { get; set; }
+
+        public ushort ChannelPeriod { get; set; }
+
+        public ushort DeviceNumber { get; set; }
+
+        public void Open()
+        {
+        }
     }
 
     /*
