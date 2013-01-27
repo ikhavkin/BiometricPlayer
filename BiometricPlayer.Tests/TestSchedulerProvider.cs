9c8530379e6d5e18bcaf3623ca38619c0ef04e7e
Igor Khavkin <ikhavkin@gmail.com>
2013-01-27 20:12:51 +0200

    autofac reference and simple test scheduler provider

---
+++ BiometricPlayer.Tests/TestSchedulerProvider.cs
@@ -0,0 +1,49 @@
+﻿using System.Reactive.Concurrency;
+using BiometricPlayer.Core;
+using Microsoft.Reactive.Testing;
+
+namespace BiometricPlayer.Tests
+{
+    /// <summary>
+    /// Test scheduler provider with <see cref="TestScheduler"/> instance for each scheduler type.
+    /// </summary>
+    public class TestSchedulerProvider : ISchedulerProvider
+    {
+        readonly IScheduler currentThreadProvider = new TestScheduler();
+        readonly IScheduler dispatcherProvider = new TestScheduler();
+        readonly IScheduler immediateProvider = new TestScheduler();
+        readonly IScheduler newThreadProvider = new TestScheduler();
+        readonly IScheduler threadPoolProvider = new TestScheduler();
+        readonly IScheduler taskPoolProvider = new TestScheduler();
+
+        IScheduler ISchedulerProvider.CurrentThread
+        {
+            get { return currentThreadProvider; }
+        }
+
+        IScheduler ISchedulerProvider.Dispatcher
+        {
+            get { return dispatcherProvider; }
+        }
+
+        IScheduler ISchedulerProvider.Immediate
+        {
+            get { return immediateProvider; }
+        }
+
+        IScheduler ISchedulerProvider.NewThread
+        {
+            get { return newThreadProvider; }
+        }
+
+        IScheduler ISchedulerProvider.ThreadPool
+        {
+            get { return threadPoolProvider; }
+        }
+
+        IScheduler ISchedulerProvider.TaskPool
+        {
+            get { return taskPoolProvider; }
+        }
+    }
+}
